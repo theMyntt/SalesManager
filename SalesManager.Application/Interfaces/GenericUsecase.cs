b62cf1927fab15da83889c192c444ad21a1b1eab
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-28 19:45:20 -0300

    chore: generic usecase abstract class

---
+++ SalesManager.Application/Interfaces/GenericUsecase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SalesManager.Application.Interfaces
+{
+	public abstract class GenericUsecase<I, O>
+	{
+		public GenericUsecase()
+		{
+		}
+
+		public Task<O> Perform(I input)
+		{
+			return ApplyInteralLogic(input);
+		}
+
+		protected abstract Task<O> ApplyInteralLogic(I input);
+	}
+}
+
+
