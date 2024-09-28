591ca32b6ea9455ef62ea2bd3fc9976988b1ba87
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-27 22:56:32 -0300

    chore: db context

---
+++ SalesManager.Infra.Data/Context/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SalesManager.Domain.Entities;
+
+namespace SalesManager.Infra.Data.Context
+{
+	public class DatabaseContext : DbContext
+	{
+		public DatabaseContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			modelBuilder.Entity<User>().ToTable("TBL_USUARIOS");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
