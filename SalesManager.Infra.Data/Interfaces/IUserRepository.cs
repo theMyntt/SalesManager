d620de982da47c2452cb45731c92e3e7229760a3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-28 19:11:32 -0300

    feat: user repository

---
+++ SalesManager.Infra.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SalesManager.Domain.Entities;
+
+namespace SalesManager.Infra.Data.Interfaces
+{
+	public interface IUserRepository
+	{
+        Task<User> Login(string userName, string password);
+    }
+}
+
---
+++ SalesManager.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SalesManager.Domain.Entities;
+using SalesManager.Infra.Data.Context;
+using SalesManager.Infra.Data.Interfaces;
+
+namespace SalesManager.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRepository(DatabaseContext context) => _context = context;
+
+        public async Task<User> Login(string userName, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName && u.Password == password) ?? throw new InvalidDataException("No User Found");
+
+            return user;
+        }
+    }
+}
+
