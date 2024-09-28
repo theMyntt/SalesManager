4274c106164cd34145a0ed1aba0caab3f021368e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-28 19:45:36 -0300

    feat: login internal logic

---
+++ SalesManager.Application/DTOs/UserLoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesManager.Application.DTOs
+{
+	public class UserLoginDTO
+	{
+		[Required]
+		public required string Username { get; set; }
+
+		[Required]
+		public required string Password { get; set; }
+	}
+}
+
---
+++ SalesManager.Application/Usecases/UserLoginUsecase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SalesManager.Application.DTOs;
+using SalesManager.Application.Interfaces;
+using SalesManager.Domain.Schemas;
+using SalesManager.Infra.Data.Interfaces;
+
+namespace SalesManager.Application.Usecases
+{
+    public class UserLoginUsecase : GenericUsecase<UserLoginDTO, UserLoginResponse>
+    {
+        private readonly IUserRepository _repository;
+
+        public UserLoginUsecase(IUserRepository repository) => _repository = repository;
+
+        protected override async Task<UserLoginResponse> ApplyInteralLogic(UserLoginDTO input)
+        {
+            var user = await _repository.Login(input.Username, input.Password);
+
+            if (user == null)
+            {
+                return new UserLoginResponse
+                {
+                    Message = "No user found",
+                    StatusCode = 404,
+                    User = null
+                };
+            }
+
+            return new UserLoginResponse
+            {
+                Message = "User found",
+                StatusCode = 200,
+                User = new Domain.Entities.User(
+                    user.Id,
+                    user.Name,
+                    user.UserName,
+                    user.Email,
+                    "",
+                    user.IsActive,
+                    user.CreatedAt,
+                    user.UpdatedAt
+                    )
+            };
+        }
+    }
+}
+
